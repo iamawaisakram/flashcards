c4bcab728526dbccb450b22f87aa790876381ab9
usmanx1 <119616036+usmanx1@users.noreply.github.com>
2023-11-14 19:18:20 +0500

    New Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Flashcard from './components/atoms/flashcard';
+import React from "react";
+import Flashcard from "./components/atoms/flashcard";
+import "./App.css";
 
 const App: React.FC = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mt-3 cursor-pointer hover:text-blue-500">
-        Flashcards
-      </h1>
-      <Flashcard />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mt-3 cursor-pointer hover:text-blue-500">
+                Flashcards
+            </h1>
+            <Flashcard />
+        </div>
+    );
 };
 
 export default App;
--- src/components/atoms/flashcard.tsx
+++ src/components/atoms/flashcard.tsx
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
-import '../../App.css';  // adjust the path here
+import React, { useState } from "react";
 
 const Flashcard: React.FC = () => {
-  const [isFlipped, setFlipped] = useState(false);
+    const [isFlipped, setFlipped] = useState(false);
 
-  const handleFlip = () => {
-    setFlipped(!isFlipped);
-  };
+    const handleFlip = () => {
+        setFlipped(!isFlipped);
+    };
 
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className={`card ${isFlipped ? 'flipped' : ''}`}>
-        <div className="front">
-          <p>Front Side</p>
+
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className={`card w-60 h-80 cursor-pointer  ${isFlipped ? "flipped" : ""}`}>
+                <div className="front absolute w-full h-full flex items-center justify-center shadow-md">
+
+                    <p>Front Side</p>
+                </div>
+                <div className="back absolute w-full h-full  flex items-center justify-center shadow-md">
+                    <p>Back Side</p>
+                </div>
+            </div>
+            <button
+                onClick={handleFlip}
+                className="mt-4 ms-4 p-2 bg-blue-500 text-white rounded"
+            >
+                Flip
+            </button>
         </div>
-        <div className="back">
-          <p>Back Side</p>
-        </div>
-      </div>
-      <button onClick={handleFlip} className="mt-4 ms-4 p-2 bg-blue-500 text-white rounded">
-        Flip
-      </button>
-    </div>
-  );
+    );
 };
 
 export default Flashcard;
